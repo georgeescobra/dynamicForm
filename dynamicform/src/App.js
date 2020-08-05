35f1aa73728f154b1a81ebf372a115bd9cd7dd92
georgeescobra <george@DESKTOP-A2UP370.localdomain>
2020-08-04 21:24:56 -0700

    added skeleton

---
+++ dynamicform/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import Formpage from "./components/Formpage";
+
+class App extends Component {
+  render(){
+    return (
+      <div>
+        <Formpage /> 
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ dynamicform/src/components/Formpage.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import Forms from "../components/Forms"
+
+class Formpage extends Component {
+    constructor(props){
+        super(props);
+        this.state = {};
+    }
+
+    render(){
+        return(
+            <div>
+                <Forms />
+            </div>
+        );
+    
+    }
+
+
+}
+export default Formpage;
