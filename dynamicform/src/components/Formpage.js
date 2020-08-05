5a94ae8c965acbf9178958d4acd14c5b3372838d
georgeescobra <george@DESKTOP-A2UP370.localdomain>
2020-08-04 22:45:59 -0700

    able to dynamically show conditional i.e. parental consent

--- dynamicform/src/components/Formpage.js
+++ dynamicform/src/components/Formpage.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import Forms from "../components/Forms"
-import ConfigData from "../jsonTests/test2.json" // change this to switch the dynamic form config
+import ConfigData from "../jsonTests/test3.json" // change this to switch the dynamic form config
 
 class Formpage extends Component {
     constructor(props){
@@ -8,7 +8,7 @@ class Formpage extends Component {
         this.state = {};
         this.handler = this.handler.bind(this);
         this.handleChange = this.handleChange.bind(this);
-
+        this.getParentStateValue = this.getParentStateValue.bind(this);
     }
 
     // this returns the contents of the Form
@@ -27,15 +27,25 @@ class Formpage extends Component {
         this.setState({[event.target.name] : event.target.value});
     };
 
+    getParentStateValue = (name) => {
+        if (name in this.state){
+            return this.state[name];
+        }
+        return null;
+    };
+
     render(){
         return(
             <div>
                 {/*make sure the data is wrapped in []*/}
                 { ConfigData.map((configDetail) => {
+                    let containsConditional = ("conditional" in configDetail)? true : false;
                     return <Forms 
                             config={configDetail} 
                             handler={this.handler}
                             handleChange={this.handleChange}
+                            getParentStateValue={this.getParentStateValue}
+                            containsConditional={containsConditional}
                             />
                 })}
                 <button type="button" onClick={() => this.submitButton()}>SUBMIT</button>
--- dynamicform/src/components/Forms.js
+++ dynamicform/src/components/Forms.js
@@ -4,6 +4,7 @@ class Forms extends Component {
     constructor(props){
         super(props);
         this.state = {
+            hide : true,
         };
     }
 
@@ -12,20 +13,32 @@ class Forms extends Component {
     }
 
     render(){
+        if(this.props.containsConditional){
+            this.state.hide = false;
+            const condFunc = new Function("value1", this.props.config.conditional.show_if); // creates the conditional function from the config
+            // checks to see if form value was filled by user (for test3: date of birth)
+            const val = this.props.getParentStateValue(this.props.config.conditional.name);
+            if(val){
+                this.state.hide = condFunc(val); // uses the value given by user and checks the condition
+            }
+
+        }
         return(
             <div>
-                <label>{this.props.config.human_label}</label>
-                <input
-                    type={this.props.config.type}
-                    name={this.props.config.name}
-                    onChange={this.props.handleChange}
-                />
+                {   !! this.state.hide /* if hide is false, renders the form : if true, does not render*/
+                    ?   <div>
+                            <label>{this.props.config.human_label}</label>
+                            <input
+                                type={this.props.config.type}
+                                name={this.props.config.name}
+                                onChange={this.props.handleChange}
+                            />
+                        </div>
+                    : <div />
+                }
             </div>
         );
-    
     }
-
-
 }
 
 export default Forms;
